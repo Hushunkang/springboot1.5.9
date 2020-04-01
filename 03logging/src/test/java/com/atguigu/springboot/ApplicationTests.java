d4ee8a51422394ace747137aedd751c16a8aa699
Hushunkang <2752962035@qq.com>
2020-04-02 06:30:38 +0800

    springboot logging

---
+++ 03logging/src/test/java/com/atguigu/springboot/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.atguigu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
