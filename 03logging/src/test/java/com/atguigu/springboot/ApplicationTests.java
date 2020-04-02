9e0b876dfa54e025d591825fdca04b3d6e7a595d
Hushunkang <2752962035@qq.com>
2020-04-02 11:01:41 +0800

    springboot web restful crud

--- 03logging/src/test/java/com/atguigu/springboot/ApplicationTests.java
+++ 03logging/src/test/java/com/atguigu/springboot/ApplicationTests.java
@@ -16,3 +16,32 @@ class ApplicationTests {
     }
 
 }
+
+class A{
+
+    public int age = 10;
+
+    public int getAge(){
+        return age;
+    }
+
+}
+
+class B extends A{
+
+    public int age = 5;
+
+    public int getAge(){
+        return age;
+    }
+
+    public static void main(String[] args) {
+        A o1 = new A();
+        A o2 = new B();
+        B o3 = new B();
+        System.out.println(o1.age);//10
+        System.out.println(o2.age);//属性没有重写这么一说，编译运行都看左边，因此结果10
+        System.out.println(o3.age);//5
+    }
+
+}
