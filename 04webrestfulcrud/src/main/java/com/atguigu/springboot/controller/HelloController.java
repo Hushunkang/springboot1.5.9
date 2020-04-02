ee0a90f0810bcccabdf6ee9feb57e596a1ca0c02
Hushunkang <2752962035@qq.com>
2020-04-02 14:19:49 +0800

    springboot thymeleaf模板引擎

--- 04webrestfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ 04webrestfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * @author hskBeginner Email：2752962035@qq.com
@@ -9,12 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  * @create 2020年04月02日 11时55分00秒
  */
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello，努力学习，认真工作。。。";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","你好...");
+        //classpath:/templates/success.html
+        return "success";
     }
 
 }
