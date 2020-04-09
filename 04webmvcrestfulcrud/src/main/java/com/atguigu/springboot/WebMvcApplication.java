16cda6375abb0c5613cb6246fc98282219383d33
Hushunkang <2752962035@qq.com>
2020-04-09 16:38:23 +0800

    spring boot对于spring mvc自动配置原理以及全面接管spring mvc

---
+++ 04webmvcrestfulcrud/src/main/java/com/atguigu/springboot/WebMvcApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月09日 16时30分58秒
+ */
+@SpringBootApplication
+public class WebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcApplication.class,args);
+    }
+
+}
---
+++ 04webmvcrestfulcrud/src/test/java/com/atguigu/springboot/WebMvcApplicationTests.java
@@ -0,0 +1,20 @@
+package com.atguigu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月09日 16时36分17秒
+ */
+@SpringBootTest
+public class WebMvcApplicationTests {
+
+    @Test
+    public void test01(){
+
+    }
+
+}
