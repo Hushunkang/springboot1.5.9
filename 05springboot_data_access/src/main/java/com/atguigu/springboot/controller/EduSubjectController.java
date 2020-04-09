feb130b770fb45d608b708a729dcdeceaaf6abb9
Hushunkang <2752962035@qq.com>
2020-04-10 01:06:45 +0800

    spring boot整合mybatis xml配置版本

---
+++ 05springboot_data_access/src/main/java/com/atguigu/springboot/controller/EduSubjectController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springboot.controller;
+
+import com.atguigu.springboot.bean.EduSubject;
+import com.atguigu.springboot.mapper.EduSubjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月10日 00时47分15秒
+ */
+@RestController
+@RequestMapping("/subject")
+public class EduSubjectController {
+
+    @Autowired
+    private EduSubjectMapper eduSubjectMapper;
+
+    @GetMapping("getSubjectById/{id}")
+    public EduSubject getSubjectById(@PathVariable("id") String id){
+        return eduSubjectMapper.getSubjectById(id);
+    }
+
+}
--- 05springboot_data_access/src/main/java/com/atguigu/springboot/mapper/EduSubjectMapper.java
+++ 05springboot_data_access/src/main/java/com/atguigu/springboot/mapper/EduSubjectMapper.java
@@ -1,5 +1,7 @@
 package com.atguigu.springboot.mapper;
 
+import com.atguigu.springboot.bean.EduSubject;
+
 /**
  * @author hskBeginner Email：2752962035@qq.com
  * @version 1.0
@@ -11,4 +13,7 @@ package com.atguigu.springboot.mapper;
 //底层jdk动态代理生成了这些接口的实现类，生成了代理的对象实例（真正工作的是这个）
 //并且将这个bean注册到spring ioc容器里面
 public interface EduSubjectMapper {
+
+    EduSubject getSubjectById(String id);
+
 }
