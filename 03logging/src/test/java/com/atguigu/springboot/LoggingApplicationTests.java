00bbe2266dfad520d057541387291dd93968c90b
Hushunkang <2752962035@qq.com>
2020-04-09 14:48:30 +0800

    项目中日志输出一定要实事求是，讲究规范。。。

--- 03logging/src/test/java/com/atguigu/springboot/LoggingApplicationTests.java
+++ 03logging/src/test/java/com/atguigu/springboot/LoggingApplicationTests.java
@@ -1,6 +1,8 @@
 package com.atguigu.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -13,9 +15,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class LoggingApplicationTests {
 
+    //日志记录器
+//    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());
+//    private Logger logger = LoggerFactory.getLogger(LoggingApplicationTests.class);
+
     @Test
     public void test01(){
-
+        //日志级别由低到高，日志级别越低，最终打印的信息越多(*￣︶￣)
+        logger.trace("这是trace日志。。。");
+        logger.debug("这是debug日志。。。");
+        //spring boot2.2.1版本默认的日志级别为info级别
+        logger.info("这是info日志。。。");
+        logger.warn("这是warn日志。。。");
+        logger.error("这是error日志。。。");
     }
 
 }
